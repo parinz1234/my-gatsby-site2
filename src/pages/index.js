e2dd9e72907a4dde118d771e78755c04c4a0827c
Parinya Onsuwan <parinya.ons@gmail.com>
2018-06-30 01:57:45 +0700

    transform and create page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,73 @@
 import React from "react";
+import styled from "styled-components";
+import Link from "gatsby-link";
 
-export default () => (
-  <div>
-    <h1>Amazing Pandas Eating Things</h1>
+import { rhythm } from "../utils/typography";
+
+const Title = styled.h1`
+  display: inline-block,
+  border-bottom: 1px solid;
+`
+
+const MyLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const ContentTitle = styled.h3`
+  margin-bottom: ${ rhythm(1 / 4) };
+`
+
+const SpanDate = styled.span`
+  color: #BBB;
+`
+
+export default ({ data }) => {
+  return (
     <div>
-      <img
-        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-        alt="Group of pandas eating bamboo"
-      />
+      <Title>
+        Amazing Pandas Eating Things
+      </Title>
+      <h4>
+        {data.allMarkdownRemark.totalCount} Posts
+      </h4>
+      {data.allMarkdownRemark.edges.map(({ node }) =>
+        <div key={node.id}>
+          <MyLink
+            to={node.fields.slug}
+          >
+
+            <ContentTitle>
+              {node.frontmatter.title}{" "}
+              <SpanDate>— {node.frontmatter.date}</SpanDate>
+            </ContentTitle>
+            <p>
+              {node.excerpt}
+            </p>
+          </MyLink>
+        </div>
+      )}
     </div>
-  </div>
-);
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
